438e9233c9360fad359c33417e9a971442369ab5
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-11-06 23:34:55 +0000

    adding log in, log out to profiles

--- gym_share/profiles/serializers.py
+++ gym_share/profiles/serializers.py
@@ -3,11 +3,12 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    is_owner = serializers.SerializerMethodField()
 
     
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image'
+            'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image','is_owner',
         ]
--- gym_share/profiles/urls.py
+++ gym_share/profiles/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from profiles import views
 
+
 urlpatterns = [
-    path('profiles/', views.ProfileList.as_view()),
-    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
+    path('profiles/', views.ProfileView.as_view()),
+    path('profiles/<int:pk>/', views.ProfileDetailView.as_view())
 ]
- 
