e75e727140977d46990f9cd0b36cac57a93cbefb
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-11-06 23:49:21 +0000

    adding posts model

--- gym_share/profiles/serializers.py
+++ gym_share/profiles/serializers.py
@@ -5,6 +5,11 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
 
+
+    def get_is_owner(self, obj):
+        request = self.context.get('request')
+        return request.user == obj.owner
+
     
 
     class Meta:
