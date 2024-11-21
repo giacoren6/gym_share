8ccfed215592f865ca2c611cbd9c575d863969d3
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-11-21 14:36:55 +0000

    preparing apps for deployment

--- gym_share/profiles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from profiles import views
-
-
-urlpatterns = [
-    path('profiles/', views.ProfileView.as_view()),
-    path('profiles/<int:pk>/', views.ProfileDetailView.as_view())
-]
