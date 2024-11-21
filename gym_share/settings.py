fa272fc18869084891e94370a373ade048847729
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-11-21 21:56:57 +0000

    Update settings.py

--- gym_share/settings.py
+++ gym_share/settings.py
@@ -44,9 +44,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['https://dashboard.heroku.com/apps/gym-share']
+#ALLOWED_HOSTS = ['https://dashboard.heroku.com/apps/gym-share']
 #ALLOWED_HOSTS = ['*']
-#ALLOWED_HOSTS = ['gym-share-9a40a7748e0a.herokuapp.com']
+ALLOWED_HOSTS = ['gym-share-9a40a7748e0a.herokuapp.com']
 
 # Application definition
 
