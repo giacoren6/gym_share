fc1a97aa5f19f3d8da80135ffdd676c6d1f97ccd
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-11-21 22:32:08 +0000

    Update env.py

--- gym_share/env.py
+++ gym_share/env.py
@@ -2,6 +2,6 @@ import os
 
 os.environ['CLOUDINARY_URL'] = 'cloudinary://<128889433648643>:<GbGfhe5Nw91kJS9z5cJzIOgqYVQ>@drkawlebq'
 #os.environ['DATABASE_URL'] = "postgres://ueb2xhhf7ly:BUA5ir5VX0Ke@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/drum_heat_e_568873"
-os.environ['DATABASE_URL'] = postgres://ueb2xhhf7ly:BUA5ir5VX0Ke@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/gave_spree_shun_611585
+os.environ['DATABASE_URL'] = 'postgres://ueb2xhhf7ly:BUA5ir5VX0Ke@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/gave_spree_shun_611585'
 os.environ['SECRET_KEY'] = 'Tekken'
 os.environ['DEV'] = '1'
