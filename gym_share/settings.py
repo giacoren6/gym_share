0cc75d2ca569edd91a97183995341e55047dd7c0
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-11-21 22:14:47 +0000

    Update settings.py

--- gym_share/settings.py
+++ gym_share/settings.py
@@ -40,8 +40,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-#DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
+#DEBUG = True
 
 
 #ALLOWED_HOSTS = ['https://dashboard.heroku.com/apps/gym-share']
