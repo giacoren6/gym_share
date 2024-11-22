0fc3e75eb0c914552f0d9957cc958f1eef4ad95a
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-11-22 12:42:46 +0000

    Update settings.py

--- gym_share/settings.py
+++ gym_share/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 import dj_database_url
 import django_heroku
 
@@ -34,8 +35,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+from django.core.management.utils import get_random_secret_key
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
+
+
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 #SECRET_KEY = 'Tekken'
 
 # SECURITY WARNING: don't run with debug turned on in production!
