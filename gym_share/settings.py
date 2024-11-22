709052fdabdc1de704ec3a843dc31145b1758b90
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-11-22 19:15:21 +0000

    Reset database and remade migrations

--- gym_share/settings.py
+++ gym_share/settings.py
@@ -160,8 +160,7 @@ WSGI_APPLICATION = "gym_share.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-import dj_database_url
-import os
+
 
 DATABASES = {
     'default': {
